ceaf5e8a2cb34d5a2b0af291a03f813a0b7af438
vomaingoc <vomaingoc@gmail.com>
2018-10-16 13:09:47 +0700

    ..

--- XeViet/src/screens/TabScreen.js
+++ XeViet/src/screens/TabScreen.js
@@ -14,35 +14,11 @@ import {
 import styles from "../styles/styles";
 import variables from "../styles/variables";
 import Ionicons from "react-native-vector-icons/Ionicons";
-//Icon tabbar navigation
-const icon_tabbar_home = "../images/icon_tabbar_home.png";
-const icon_tabbar_home_active = "../images/icon_tabbar_home_active.png";
-
-const icon_tabbar_ch = "../images/icon_tabbar_ch.png";
-const icon_tabbar_ch_active = "../images/icon_tabbar_ch_active.png";
-
-const icon_tabbar_dh = "../images/icon_tabbar_dh.png";
-const icon_tabbar_dh_active = "../images/icon_tabbar_dh_active.png";
-
-const icon_tabbar_news = "../images/icon_tabbar_news.png";
-const icon_tabbar_news_active = "../images/icon_tabbar_news_active.png";
-
-const icon_tabbar_notification = "../images/icon_tabbar_notification.png";
-const icon_tabbar_notification_active = "../images/icon_tabbar_notification_active.png";
-
-const icon_tabbar_more = "../images/icon_tabbar_more.png";
-const icon_tabbar_more_active = "../images/icon_tabbar_more_active.png";
-
-const icon_tabbar_car = "../images/icon_tabbar_car.png";
-const icon_tabbar_car_active = "../images/icon_tabbar_car_active.png";
 
 let tabBgColor = variables.color.tabNavBgc,
   tabActiveBgColor = variables.color.tabNavActiveBgc,
   tabBarTextColor = variables.color.tabNav,
   tabBarActiveTextColor = variables.color.tabNavActive;
-
-let fz = 10;
-
 export default class TabScreen extends Component<Props> {
   constructor(props) {
     super(props);
